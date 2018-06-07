a196dec4c27d312ee3ac846845df9452f7457b3d
nunofnrodrigues1987 <40035718+nunofnrodrigues1987@users.noreply.github.com>
2018-06-07 18:01:26 +0100

    Add files via upload

--- jenkins-pipelines/E2E-automation-test.groovy
+++ jenkins-pipelines/E2E-automation-test.groovy
@@ -1,7 +1,8 @@
 #!groovy
 node {
     stage('Git checkout') { // for display purposes
-        git 'https://github.com/BushnevYuri/e2e-automation-pipeline.git'
+        //git 'https://github.com/BushnevYuri/e2e-automation-pipeline.git'
+        git 'https://github.com/nunofnrodrigues1987/sandbox'
     }
     stage('Smoke') {
         try {
